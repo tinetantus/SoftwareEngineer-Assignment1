09a961f212e6fe4b6f4b781023dfaac5ddfb4d18
DominicVerschoor <dominic.verschoor@student.maastrichtuniversity.nl>
2022-04-20 12:01:32 +0200

    comment

--- GeneralPackage/Entry/EntryInterface.java
+++ GeneralPackage/Entry/EntryInterface.java
@@ -5,13 +5,14 @@ import java.util.List;
 
 public interface EntryInterface {
 
-    public List<String> getMenuList();
-    public void addToMenu(String name);
-    public void placeOrder();
+    public List<String> getMenuList(); // Get the menu from the kitchen
+    public void addToMenu(String name); // Adds items to customers orders through a string (name of dish)
+    public void placeOrder(); // Customer finalizes their order and sends it to kitchen through this method
 
-    public double getPaymentSummary();
-    public void makePayment(double amount);
-    public double getWaitTime();
-    public void setDistance(double distance);
+    public double getPaymentSummary(); // Returns the billing (including pricing)
+    public void makePayment(double amount); // Customer makes payment that is sent to the ktichen
+                                            //(this happens in placeOrder() or takeaway and delivery)
+    public double getWaitTime(); //Retruns the waiting time for the dish (cooking + travel time)
+    public void setDistance(double distance); // Distance is needed to determine travel time
 
 }
--- GeneralPackage/Kitchen/MenuItem/MenuItemInterface.java
+++ GeneralPackage/Kitchen/MenuItem/MenuItemInterface.java
@@ -2,11 +2,11 @@ package GeneralPackage.Kitchen.MenuItem;
 
 public interface MenuItemInterface {
 
-    public double getPrepTime();
+    public double getPrepTime(); // Returns the time to prepare dish
 
-    public double getMenuPrice();
+    public double getMenuPrice(); // Returns the price of the dish
 
-    public String getItemName();
+    public String getItemName(); // Retruns name of dish
 
-    public String getType();
+    public String getType(); // Returns the type of dish (main, side, dessert, drink)
 }
